1fe3ab566eeb6b481ea75976822e41a338ec0347
unknown <carlos.barreraacuna@gmail.com>
2025-02-17 18:49:57 -0500

    config /api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,15 +6,23 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
         $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+
+
+        $middleware->group('api', [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, // Para solicitudes con estado (SPA)
+            'throttle:api', // Limitar solicitudes
+            \Illuminate\Routing\Middleware\SubstituteBindings::class, // Resolución de rutas
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+
